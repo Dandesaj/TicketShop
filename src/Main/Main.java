31a81d94bef24c79da6782e515d9780069d8069d
Sebastian Munoz Hilton <sebajhilton@gmail.com>
2021-10-01 12:59:26 -0300

    primera vuelta y probando el merge

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,19 @@
 package Main;
-
+import Modelo.Evento;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		List<Evento> eventos;
+		//Simulador de venta de ticket
+		
+		//Caso 1 : crear evento
+		Evento poolParty = new Evento("PoolParty",18,false);
+		
+		//Caso2 : Venta de entrada
+		Vendedor mrVenta = new Vendedor("mrVenta","19.201.564-6","9-")
+		
 	}
 
 }
--- src/Modelo/EntradaVip.java
+++ src/Modelo/EntradaVip.java
@@ -1,5 +1,15 @@
 package Modelo;
 
 public class EntradaVip extends Entrada {
+	private boolean Regalo;
 
+	public boolean isRegalo() {
+		return Regalo;
+	}
+
+	public void setRegalo(boolean regalo) {
+		Regalo = regalo;
+	}
+	
+	
 }
